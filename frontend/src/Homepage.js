3b3f6346ad48ae9923f6351ea89f43497d3e951a
Matt Smida <mcs@m4t7.com>
2024-03-25 11:46:47 -0400

    tidied homepage links

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
-import userContext from './userContext';
 import { Link } from 'react-router-dom';
+
+import userContext from './userContext';
+import "./Homepage.css";
 
 /** Renders the homepage of the app
  *
@@ -22,14 +24,16 @@ function Homepage() {
 
   return (
     <div className="Homepage">
+      {/* TODO: background image */}
       <h1>Jobly</h1>
       <p>All the jobs in one, convenient place.</p>
       {currUser
         ? <h2>Welcome back, {currUser.firstName}!</h2>
         : (
           <div>
-            <Link to="/login">Log in</Link>
             <Link to="/signup">Sign up</Link>
+            &nbsp;&nbsp;&nbsp;
+            <Link to="/login">Log in</Link>
           </div>
         )}
     </div>
